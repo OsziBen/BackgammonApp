7977e83700f8eaf63ceba70fd2097b9e15547328
OsziBen <osziben2@gmail.com>
2025-12-06 22:04:46 +0100

    Add group role model, configure nav. properties

---
+++ Domain/GroupRole/GroupRole.cs
@@ -0,0 +1,18 @@
+﻿using Common.Models;
+
+namespace Domain.GroupRole
+{
+    public class GroupRole : BaseEntity
+    {
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public Guid? GroupId { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset LastUpdatedAt { get; set; }
+        public DateTimeOffset? DeletedAt { get; set; }
+
+        public Group.Group? Group { get; set; }
+        public ICollection<GroupMembershipRole.GroupMembershipRole> GroupMembershipRoles { get; set; } = [];
+    }
+}
