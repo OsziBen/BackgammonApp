7a103e86d17153dc79e9254c25cdad46f0b22c78
OsziBen <osziben2@gmail.com>
2025-12-07 00:29:30 +0100

    Add reaction model, configure nav. properties

---
+++ Domain/Reaction/Reaction.cs
@@ -0,0 +1,19 @@
+﻿using Common.Enums.Reaction;
+using Common.Models;
+
+namespace Domain.Reaction
+{
+    public class Reaction : BaseEntity
+    {
+        public Guid AuthorId { get; set; }
+        public Guid TargetId { get; set; }
+        public ReactionType Type { get; set; }
+        public ReactionTargetType TargetType { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset LastUpdatedAt { get; set; }
+        public DateTimeOffset? DeletedAt { get; set; }
+
+        public User.User Author { get; set; } = null!;
+    }
+}
