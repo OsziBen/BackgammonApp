974ed0503774e81116967d3defd05cd9e94b915e
OsziBen <osziben2@gmail.com>
2025-12-07 00:29:19 +0100

    Add comment model, configure nav. properties

---
+++ Domain/Comment/Comment.cs
@@ -0,0 +1,21 @@
+﻿using Common.Models;
+
+namespace Domain.Comment
+{
+    public class Comment : BaseEntity
+    {
+        public Guid AuthorId { get; set; }
+        public Guid PostId { get; set; }
+        public Guid? ParentCommentId { get; set; }
+        public required string Content { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset LastUpdatedAt { get; set; }
+        public DateTimeOffset? DeletedAt { get; set; }
+
+        public User.User Author { get; set; } = null!;
+        public Post.Post Post { get; set; } = null!;
+        public Comment? ParentComment { get; set; }
+        public ICollection<Comment> Replies { get; set; } = [];
+    }
+}
