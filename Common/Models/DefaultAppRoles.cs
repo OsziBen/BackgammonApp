1767a24cd79554f7fd6014e3a964e13df576cd89
OsziBen <osziben2@gmail.com>
2025-12-06 22:03:03 +0100

    Add default app roles class with unique IDs

---
+++ Common/Models/DefaultAppRoles.cs
@@ -0,0 +1,8 @@
+﻿namespace Common.Models
+{
+    public static class DefaultAppRoles
+    {
+        public static readonly Guid AdminRoleId = new Guid("10000000-0000-0000-0000-000000000001");
+        public static readonly Guid UserRoleId = new Guid("10000000-0000-0000-0000-000000000002");
+    }
+}
