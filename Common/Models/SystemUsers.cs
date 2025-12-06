14ed62f9ac511aae0b9e86cc63ef813dd3958683
OsziBen <osziben2@gmail.com>
2025-12-06 22:03:32 +0100

    Add default system users class with unique IDs

---
+++ Common/Models/SystemUsers.cs
@@ -0,0 +1,9 @@
+﻿namespace Common.Models
+{
+    public static class SystemUsers
+    {
+        public static readonly Guid System = new Guid("00000000-0000-0000-0000-000000000001");
+        public static readonly Guid DeletedUser = new Guid("00000000-0000-0000-0000-000000000002");
+        public static readonly Guid SystemAdmin = new Guid("00000000-0000-0000-0000-000000000003");
+    }
+}
