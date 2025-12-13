e456cd4dd81adc6d9ab619b8f61b26dd3db09c00
OsziBen <osziben2@gmail.com>
2025-12-14 00:44:11 +0100

    Add DiceRoll model, configure properties

---
+++ Domain/DiceRoll/DiceRoll.cs
@@ -0,0 +1,17 @@
+﻿using Common.Models;
+
+namespace Domain.DiceRoll
+{
+    public class DiceRoll : BaseEntity
+    {
+        public Guid PlayerTurnId { get; set; }
+
+        public int Die1 { get; set; }
+        public int Die2 { get; set; }
+        public bool IsDouble { get; set; }
+
+        public DateTimeOffset CreatedAt { get; set; }
+
+        public PlayerTurn.PlayerTurn PlayerTurn { get; set; } = null!;
+    }
+}
