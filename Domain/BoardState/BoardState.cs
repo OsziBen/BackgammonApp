5924366bcb92e59da06b242ec4635243a0c59ba4
OsziBen <osziben2@gmail.com>
2025-12-14 00:44:47 +0100

    Add BoardState model, configure properties

---
+++ Domain/BoardState/BoardState.cs
@@ -0,0 +1,27 @@
+﻿using Common.Enums.BoardState;
+using Common.Models;
+using Domain.PlayerTurn;
+
+namespace Domain.BoardState
+{
+    public class BoardState : BaseEntity
+    {
+        public Guid GameId { get; set; }
+        public Guid? PlayerTurnId { get; set; }
+        public Guid? CheckerMoveId { get; set; }
+
+        public required string PositionsJson { get; set; }
+        public int Order { get; set; }
+
+        public int BarWhite { get; set; }
+        public int BarBlack { get; set; }
+        public int OffWhite { get; set; }
+        public int OffBlack { get; set; }
+
+        public PlayerColor CurrentPlayer { get; set; }
+        public int CubeValue { get; set; }
+        public Guid? CubeOwnerId { get; set; }
+
+        public DateTimeOffset CreatedAt { get; set; }
+    }
+}
