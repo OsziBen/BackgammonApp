5cdb2a31e21f696cb880ebba11e02ad058b3368b
OsziBen <osziben2@gmail.com>
2025-12-14 00:46:00 +0100

    Add result type enum for game model

---
+++ Common/Enums/Game/GameResultType.cs
@@ -0,0 +1,10 @@
+﻿namespace Common.Enums.Game
+{
+    public enum GameResultType
+    {
+        Defeat = 0,
+        SimpleVictory = 1,
+        GammonVictory = 2,
+        BackgammonVictory = 3
+    }
+}
