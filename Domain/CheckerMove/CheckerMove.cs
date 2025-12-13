1c98db7a68413fbabcb3ebbcf197f2ba92b5afd7
OsziBen <osziben2@gmail.com>
2025-12-14 00:44:30 +0100

    Add CheckerMove model, configure properties

---
+++ Domain/CheckerMove/CheckerMove.cs
@@ -0,0 +1,23 @@
+﻿using Common.Models;
+
+namespace Domain.CheckerMove
+{
+    public class CheckerMove : BaseEntity
+    {
+        public Guid PlayerTurnId { get; set; }
+        public int OrderWithinTurn { get; set; }  // hányadik lépés adott körben
+        
+        public int FromPoint { get; set; }
+        public int ToPoint { get; set; }        
+        public int PipsUsed { get; set; }    // érték, nem a kocka száma!
+
+        public bool IsHit { get; set; }
+        public bool IsBearOff { get; set; }
+
+        public Guid? ActualBoardStateId { get; set; }
+
+        public DateTimeOffset CreatedAt { get; set; }
+
+        public PlayerTurn.PlayerTurn PlayerTurn { get; set; } = null!;
+    }
+}
