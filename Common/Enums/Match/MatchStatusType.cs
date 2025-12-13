630d4b9e429962d5e61c15890bc550a441bc3680
OsziBen <osziben2@gmail.com>
2025-12-14 00:45:26 +0100

    Add status type enum for match model

---
+++ Common/Enums/Match/MatchStatusType.cs
@@ -0,0 +1,12 @@
+﻿namespace Common.Enums.Match
+{
+    public enum MatchStatusType
+    {
+        None = 0,
+        Scheduled = 1,
+        Active = 2,
+        Finished = 3,
+        Abandoned = 4,
+        Cancelled = 5
+    }
+}
