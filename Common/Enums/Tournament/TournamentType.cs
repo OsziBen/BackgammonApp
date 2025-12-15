441bea3e0ddb1f070e632bec57cf4ec283b22bad
OsziBen <osziben2@gmail.com>
2025-12-15 22:41:59 +0100

    Add type enum for tournament

---
+++ Common/Enums/Tournament/TournamentType.cs
@@ -0,0 +1,10 @@
+﻿namespace Common.Enums.Tournament
+{
+    public enum TournamentType
+    {
+        None = 0,
+        Swiss = 1,
+        SingleElimination = 2,
+        RoundRobin = 3
+    }
+}
