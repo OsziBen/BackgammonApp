597c7dc267adc18a429f087008539abd3e6271ee
OsziBen <osziben2@gmail.com>
2025-12-15 22:43:25 +0100

    Add status type enum for tournament registration

---
+++ Common/Enums/TournamentRegistration/TournamentRegistrationStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace Common.Enums.TournamentRegistration
+{
+    public enum TournamentRegistrationStatus
+    {
+        Pending = 0,
+        Confirmed = 1,
+        Withdrawn = 2,
+        Disqualified = 3
+    }
+}
