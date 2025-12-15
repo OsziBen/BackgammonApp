0d40ad6a8d0755aac61e3703a4d25145853cb56d
OsziBen <osziben2@gmail.com>
2025-12-15 22:45:07 +0100

    Add model for tourn. round, configure properties

---
+++ Domain/TournamentRound/TournamentRound.cs
@@ -0,0 +1,24 @@
+﻿using Common.Models;
+
+namespace Domain.TournamentRound
+{
+    public class TournamentRound : BaseEntity
+    {
+        public Guid TournamentId { get; set; }
+
+        public int RoundNumber { get; set; }
+        public DateTimeOffset? StartDate { get; set; }
+        public DateTimeOffset? EndDate { get; set; }
+
+        public bool IsFinished { get; set; }
+
+        public bool IsDeleted { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset LastUpdatedAt { get; set; }
+        public DateTimeOffset? DeletedAt { get; set; }
+
+
+        public Tournament.Tournament Tournament { get; set; } = null!;
+        public ICollection<TournamentPairing.TournamentPairing> Pairings { get; set; } = [];
+    }
+}
