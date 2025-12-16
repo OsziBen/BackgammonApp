89a8fd37ba33d04d92603ac8dc86b0fb47c6b511
OsziBen <osziben2@gmail.com>
2025-12-16 19:21:43 +0100

    Add read repository interface for read actions

---
+++ Application/Interfaces/IReadRepository.cs
@@ -0,0 +1,10 @@
+﻿using Common.Models;
+
+namespace Application.Interfaces
+{
+    public interface IReadRepository<T> where T : BaseEntity
+    {
+        IQueryable<T> Query(bool includeSoftDeleted = false, bool asNoTracking = true);
+        Task<T?> GetByIdAsync(Guid id, bool includeSoftDeleted = false, bool asNoTracking = true);
+    }
+}
