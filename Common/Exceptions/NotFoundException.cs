dbede4fb0e8dddbcb40c374a284a7a18c4006b25
OsziBen <osziben2@gmail.com>
2025-12-17 16:20:10 +0100

    Add not found exception with constructors

---
+++ Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,20 @@
+﻿using Common.Enums;
+
+namespace Common.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        private const FunctionCode DefaultCode = FunctionCode.ResourceNotFound;
+
+        public NotFoundException(string message)
+            : base(message, DefaultCode) { }
+
+        public NotFoundException(FunctionCode errorCode, string message)
+            : base(errorCode, message) { }
+
+        public static NotFoundException CreateForResource(string resourceName, object key)
+        {
+            return new NotFoundException($"Entity ({key}) of type '{resourceName}' not found.");
+        }
+    }
+}
