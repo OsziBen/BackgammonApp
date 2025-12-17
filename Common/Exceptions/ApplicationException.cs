44ecd9e2fa339f85d7cdec8b2b54761b6a754723
OsziBen <osziben2@gmail.com>
2025-12-17 16:18:08 +0100

    Add app exception with constructors

---
+++ Common/Exceptions/ApplicationException.cs
@@ -0,0 +1,27 @@
+﻿using Common.Enums;
+
+namespace Common.Exceptions
+{
+    public class ApplicationException : Exception, IApplicationException
+    {
+        public FunctionCode ErrorCode { get; }
+
+        protected ApplicationException(string message, FunctionCode defaultCode)
+            : base(message)
+        {
+            this.ErrorCode = defaultCode;
+        }
+
+        protected ApplicationException(FunctionCode errorCode, string message)
+            : base(message)
+        {
+            this.ErrorCode = errorCode;
+        }
+
+        protected ApplicationException(string message, FunctionCode defaultCode, Exception innerException)
+            : base(message, innerException)
+        {
+            this.ErrorCode = defaultCode;
+        }
+    }
+}
