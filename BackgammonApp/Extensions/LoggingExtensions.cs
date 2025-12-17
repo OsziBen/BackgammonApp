cf92a3eae49968fa11289810b62d58b01de571b0
OsziBen <osziben2@gmail.com>
2025-12-17 16:24:48 +0100

    Add and configure serilog logging extension

---
+++ BackgammonApp/Extensions/LoggingExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Serilog;
+
+namespace WebAPI.Extensions
+{
+    public static class LoggingExtensions
+    {
+        public static WebApplicationBuilder ConfigureSerilog(
+            this WebApplicationBuilder builder)
+        {
+            builder.Host.UseSerilog((context, services, loggerConfig) =>
+            {
+                loggerConfig
+                    .ReadFrom.Configuration(context.Configuration)
+                    .Enrich.FromLogContext();
+            });
+
+            return builder;
+        }
+    }
+}
