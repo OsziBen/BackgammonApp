0cb470d67aff93885a896b9e1ae8c720cf651b31
OsziBen <osziben2@gmail.com>
2025-12-20 01:24:55 +0100

    Add game player model, configure properties

---
+++ Domain/GamePlayer/GamePlayer .cs
@@ -0,0 +1,20 @@
+﻿using Common.Enums.BoardState;
+using Common.Models;
+
+namespace Domain.GamePlayer
+{
+    public class GamePlayer : BaseEntity
+    {
+        public Guid GameSessionId { get; set; }
+        public Guid UserId { get; set; }
+
+        public bool IsHost { get; set; }
+        public PlayerColor Color { get; set; }
+
+        public bool IsConnected { get; set; }
+        public DateTimeOffset? LastConnectedAt { get; set; }
+
+        public GameSession.GameSession GameSession { get; set; } = null!;
+        public User.User User { get; set; } = null!;
+    }
+}
