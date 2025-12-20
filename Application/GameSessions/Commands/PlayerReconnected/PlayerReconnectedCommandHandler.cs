67b650836842d53f1bec028ea3048224846af394
OsziBen <osziben2@gmail.com>
2025-12-20 23:46:04 +0100

    Add command handler for player reconnect

---
+++ Application/GameSessions/Commands/PlayerReconnected/PlayerReconnectedCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using Application.GameSessions.Realtime;
+using Application.Interfaces;
+using MediatR;
+
+namespace Application.GameSessions.Commands.PlayerReconnected
+{
+    public class PlayerReconnectedCommandHandler : IRequestHandler<PlayerReconnectedCommand>
+    {
+        private readonly IUnitOfWork _uow;
+        private readonly IGameSessionNotifier _gameSessionNotifier;
+
+        public PlayerReconnectedCommandHandler(
+            IUnitOfWork uow,
+            IGameSessionNotifier gameSessionNotifier)
+        {
+            _uow = uow;
+            _gameSessionNotifier = gameSessionNotifier;
+        }
+
+        public async Task Handle(
+            PlayerReconnectedCommand request,
+            CancellationToken cancellationToken)
+        {
+            var player = await _uow.GamePlayers
+                .GetByIdAsync(request.GamePlayerId, asNoTracking: false);
+
+            if (player == null || player.IsConnected)
+            {
+                return;
+            }
+
+            player.IsConnected = true;
+            player.LastConnectedAt = DateTime.UtcNow;
+
+            await _uow.CommitAsync();
+
+            await _gameSessionNotifier.PlayerReconnected(
+                player.GameSessionId,
+                player.Id,
+                player.LastConnectedAt.Value);
+        }
+    }
+}
