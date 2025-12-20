2d7a35f373877f2e1207723b3150842d2b7d8f81
OsziBen <osziben2@gmail.com>
2025-12-20 23:44:33 +0100

    Add command for joining game session

---
+++ Application/GameSessions/Commands/JoinGameSession/JoinGameSessionCommand.cs
@@ -0,0 +1,11 @@
+﻿using Application.GameSessions.Responses;
+using MediatR;
+
+namespace Application.GameSessions.Commands.JoinGameSession
+{
+    public record JoinGameSessionCommand(
+        string SessionCode,
+        Guid UserId,
+        string ConnectionId
+        ) : IRequest<GameSessionSnapshotResponse>;
+}
