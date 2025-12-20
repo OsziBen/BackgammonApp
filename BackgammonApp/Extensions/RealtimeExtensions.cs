d7674c600112b696d0c56791e1c3734cc22c2104
OsziBen <osziben2@gmail.com>
2025-12-20 23:38:28 +0100

    Add extension method for realtime service config

---
+++ BackgammonApp/Extensions/RealtimeExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Application.GameSessions.Realtime;
+using Application.Realtime.Connections;
+using Infrastructure.BackgroundServices;
+using Infrastructure.Realtime.Connections;
+using WebAPI.Realtime;
+
+namespace WebAPI.Extensions
+{
+    public static class RealtimeExtensions
+    {
+        public static IServiceCollection AddRealtimeServices(this IServiceCollection services)
+        {
+            services.AddSignalR(options =>
+            {
+                options.EnableDetailedErrors = true;
+
+                options.KeepAliveInterval = TimeSpan.FromSeconds(15);
+                options.ClientTimeoutInterval = TimeSpan.FromSeconds(30);
+            });
+
+            services.AddSingleton<IConnectionMapping, InMemoryConnectionMapping>();
+
+            services.AddHostedService<DisconnectedPlayerMonitor>();
+
+            services.AddScoped<IGameSessionNotifier, SignalRGameSessionNotifier>();
+
+            return services;
+        }
+    }
+}
