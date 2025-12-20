d30c2666d7b9e7c263e2dcbd1bc715324b40d7ef
OsziBen <osziben2@gmail.com>
2025-12-20 23:42:54 +0100

    Add interface for game player repository

---
+++ Application/Interfaces/IGamePlayerRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain.GamePlayer;
+
+namespace Application.Interfaces
+{
+    public interface IGamePlayerRepository : IReadRepository<GamePlayer>, IBaseRepository<GamePlayer>
+    {
+        Task<GamePlayer?> GetBySessionAndUserAsync(
+            Guid sessionId,
+            Guid userId,
+            bool asNoTracking = false);
+
+        Task<GamePlayer?> GetOpponentAsync(
+            Guid sessionId,
+            Guid excludePlayerId,
+            bool asNoTracking = false);
+    }
+}
