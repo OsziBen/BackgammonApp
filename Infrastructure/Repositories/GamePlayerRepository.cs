c21eb7c31fcbf0814bcaf93e05a88b764f276769
OsziBen <osziben2@gmail.com>
2025-12-20 23:43:53 +0100

    Implement repositpry for game player

---
+++ Infrastructure/Repositories/GamePlayerRepository.cs
@@ -0,0 +1,34 @@
+﻿using Application.Interfaces;
+using Domain.GamePlayer;
+using Domain.User;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class GamePlayerRepository : BaseRepository<GamePlayer>, IGamePlayerRepository
+    {
+        public GamePlayerRepository(ApplicationDbContext context)
+            : base(context)
+        {
+        }
+
+        public Task<GamePlayer?> GetBySessionAndUserAsync(
+            Guid sessionId,
+            Guid userId,
+            bool asNoTracking = false)
+            => Query(asNoTracking: asNoTracking)
+                .FirstOrDefaultAsync(gp =>
+                    gp.GameSessionId == sessionId &&
+                    gp.UserId == userId);
+
+        public Task<GamePlayer?> GetOpponentAsync(
+            Guid sessionId,
+            Guid excludePlayerId,
+            bool asNoTracking = false)
+            => Query(asNoTracking: asNoTracking)
+                .FirstOrDefaultAsync(gp =>
+                    gp.GameSessionId == sessionId &&
+                    gp.UserId != excludePlayerId);
+    }
+}
