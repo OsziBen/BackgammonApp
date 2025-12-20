d117f10674e1cdcb992c911cf17ccf2f51dd4406
OsziBen <osziben2@gmail.com>
2025-12-20 23:49:45 +0100

    Add interface for persistent connection mapping

---
+++ Application/Realtime/Connections/IConnectionMapping.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Realtime.Connections
+{
+    public interface IConnectionMapping
+    {
+        void Add(string connectionId, Guid gamePlayerId);
+        bool TryGet(string connectionId, out Guid gamePlayerId);
+        void Remove(string connectionId);
+    }
+}
