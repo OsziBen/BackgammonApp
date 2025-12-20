26947bc53dcb9b1833f62abd0ecab4c2afcad352
OsziBen <osziben2@gmail.com>
2025-12-20 23:50:10 +0100

    Implement persistent connection mapping

---
+++ Infrastructure/Realtime/Connections/InMemoryConnectionMapping.cs
@@ -0,0 +1,19 @@
+﻿using Application.Realtime.Connections;
+using System.Collections.Concurrent;
+
+namespace Infrastructure.Realtime.Connections
+{
+    public class InMemoryConnectionMapping : IConnectionMapping
+    {
+        private readonly ConcurrentDictionary<string, Guid> _map = new();
+
+        public void Add(string connectionId, Guid gamePlayerId)
+            => _map[connectionId] = gamePlayerId;
+
+        public bool TryGet(string connectionId, out Guid gamePlayerId)
+            => _map.TryGetValue(connectionId, out gamePlayerId);
+
+        public void Remove(string connectionId)
+            => _map.TryRemove(connectionId, out _);
+    }
+}
