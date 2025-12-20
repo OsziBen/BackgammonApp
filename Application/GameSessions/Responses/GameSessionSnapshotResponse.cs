81b9afcad367b8a3794976dc81b614c74e05b83e
OsziBen <osziben2@gmail.com>
2025-12-20 23:48:44 +0100

    Add game session snapshot response model

---
+++ Application/GameSessions/Responses/GameSessionSnapshotResponse.cs
@@ -0,0 +1,19 @@
+﻿using Common.Enums.BoardState;
+using Common.Enums.GameSession;
+
+namespace Application.GameSessions.Responses
+{
+    public class GameSessionSnapshotResponse
+    {
+        public Guid SessionId { get; set; }
+        public Guid PlayerId { get; set; }
+        public PlayerColor PlayerColor { get; set; }
+
+        public GamePhase CurrentPhase { get; set; }
+        public Guid? CurrentPlayerId { get; set; }
+
+        public string? BoardStateJson { get; set; }
+
+        public bool IsRejoin { get; set; }
+    }
+}
