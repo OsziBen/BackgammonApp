8cfc6988efebcf572d89652d4d6d1739d678c1dd
OsziBen <osziben2@gmail.com>
2025-12-21 22:44:16 +0100

    Add game session guards to ensure global rules

---
+++ Domain/GameSession/GameSessionGuards.cs
@@ -0,0 +1,31 @@
+﻿using Common.Enums;
+using Common.Enums.GameSession;
+using Common.Exceptions;
+
+namespace Domain.GameSession
+{
+    public static class GameSessionGuards
+    {
+        public static void EnsureNotFinished(GameSession session)
+        {
+            if (session.IsFinished)
+            {
+                throw new BusinessRuleException(
+                    FunctionCode.BusinessRuleViolation,
+                    "Game session already finished");
+            }
+        }
+
+        public static void EnsureMaxPlayerCount(
+            int actual,
+            int maxAllowed)
+        {
+            if (actual > maxAllowed)
+            {
+                throw new BusinessRuleException(
+                    FunctionCode.BusinessRuleViolation,
+                     $"Player count exceeded. Max allowed: {maxAllowed}, actual: {actual}");
+            }
+        }
+    }
+}
