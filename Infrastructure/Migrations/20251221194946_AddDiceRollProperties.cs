ba8760ff9691c9d72d1416035f9b94b149a5fdb1
OsziBen <osziben2@gmail.com>
2025-12-21 22:33:56 +0100

    Migrate database changes

---
+++ Infrastructure/Migrations/20251221194946_AddDiceRollProperties.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDiceRollProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int[]>(
+                name: "LastDiceRoll",
+                table: "GameSessions",
+                type: "integer[]",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RemainingMoves",
+                table: "GameSessions",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastDiceRoll",
+                table: "GameSessions");
+
+            migrationBuilder.DropColumn(
+                name: "RemainingMoves",
+                table: "GameSessions");
+        }
+    }
+}
