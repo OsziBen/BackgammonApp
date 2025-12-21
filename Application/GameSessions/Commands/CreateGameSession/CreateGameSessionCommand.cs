b8b6501ff017a3bacf4982a3094dfafcf686d06d
OsziBen <osziben2@gmail.com>
2025-12-21 22:55:47 +0100

    Add command for creating game session

---
+++ Application/GameSessions/Commands/CreateGameSession/CreateGameSessionCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Application.GameSessions.Commands.CreateGameSession
+{
+    public record CreateGameSessionCommand(
+        Guid MatchId,
+        string SessionCode
+    ) : IRequest<Guid>;
+}
