52af821909c150eeb2d85658dfe0a75503e7892a
OsziBen <osziben2@gmail.com>
2025-12-21 22:56:45 +0100

    Add command for rolling dice

---
+++ Application/GameSessions/Commands/RollDice/RollDiceCommand.cs
@@ -0,0 +1,10 @@
+﻿using Application.GameSessions.Responses;
+using MediatR;
+
+namespace Application.GameSessions.Commands.RollDice
+{
+    public record RollDiceCommand(
+        Guid SessionId,
+        Guid PlayerId
+    ) : IRequest<RollDiceResult>;
+}
