737f8927eb708e29c7165dffa4d3f83b006b0878
OsziBen <osziben2@gmail.com>
2025-12-21 22:37:35 +0100

    Add factory for creating game player

---
+++ Domain/GamePlayer/GamePlayerFactory.cs
@@ -0,0 +1,27 @@
+﻿using Common.Enums.BoardState;
+
+namespace Domain.GamePlayer
+{
+    public static class GamePlayerFactory
+    {
+        public static GamePlayer Create(
+            Guid sessionId,
+            Guid userId,
+            bool isHost)
+        {
+            return new GamePlayer
+            {
+                Id = Guid.NewGuid(),
+                GameSessionId = sessionId,
+                UserId = userId,
+                IsHost = isHost,
+                Color = isHost
+                    ? PlayerColor.White
+                    : PlayerColor.Black,
+                IsConnected = true,
+                LastConnectedAt = DateTimeOffset.UtcNow,
+                StartingRoll = null
+            };
+        }
+    }
+}
