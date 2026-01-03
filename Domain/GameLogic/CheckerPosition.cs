dcf65327caf14e759f804172ce759f46a6c4cd7b
OsziBen <osziben2@gmail.com>
2026-01-03 21:05:09 +0100

    Add class for checker position and methods

---
+++ Domain/GameLogic/CheckerPosition.cs
@@ -0,0 +1,26 @@
+﻿using Common.Enums.BoardState;
+
+namespace Domain.GameLogic
+{
+    public class CheckerPosition
+    {
+        public PlayerColor? Owner { get; }
+        public int Count { get; }
+
+        public CheckerPosition(
+            PlayerColor? owner,
+            int count)
+        {
+            Owner = owner;
+            Count = count;
+        }
+
+        public CheckerPosition Clone()
+            => new CheckerPosition(Owner, Count);
+
+        public CheckerPosition With(
+            PlayerColor? owner,
+            int count)
+            => new CheckerPosition(Owner, count);
+    }
+}
