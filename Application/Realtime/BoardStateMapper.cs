1f59864cbdcaabeab7c475382f47ee8d99043151
OsziBen <osziben2@gmail.com>
2026-01-03 20:43:28 +0100

    Add mapper class to transfer JSON data from domain

---
+++ Application/Realtime/BoardStateMapper.cs
@@ -0,0 +1,15 @@
+﻿using Domain.GameLogic;
+using System.Text.Json;
+
+namespace Application.Realtime
+{
+    public static class BoardStateMapper
+    {
+        public static string ToJson(BoardState state)
+        {
+            var snapshot = RuntimeBoardStateSnapshot.FromDomain(state);
+
+            return JsonSerializer.Serialize(snapshot);
+        }
+    }
+}
