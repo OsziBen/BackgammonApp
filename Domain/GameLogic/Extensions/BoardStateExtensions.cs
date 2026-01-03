fbb65b355f04a39d228d677ed2f8333969aa520a
OsziBen <osziben2@gmail.com>
2026-01-03 20:48:15 +0100

    Add extension method to apply changes to board state

---
+++ Domain/GameLogic/Extensions/BoardStateExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.GameLogic.Extensions
+{
+    public static class BoardStateExtensions
+    {
+        public static BoardState Apply(
+            this BoardState state,
+            Move move)
+        {
+            return BoardStateApplier.Apply(state, move);
+        }
+    }
+}
