d2a172ab8e10bc4781a2933fc7ad5c221ee703f3
OsziBen <osziben2@gmail.com>
2026-01-03 20:41:05 +0100

    Add command for checker move action

---
+++ Application/GameSessions/MoveCheckers/MoveCheckersCommand.cs
@@ -0,0 +1,11 @@
+﻿using Application.GameSessions.Requests;
+using MediatR;
+
+namespace Application.GameSessions.MoveCheckers
+{
+    public record MoveCheckersCommand(
+        Guid SessionId,
+        Guid PlayerId,
+        IReadOnlyList<MoveDto> Moves
+        ) : IRequest<Unit>;
+}
