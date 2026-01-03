29e69e41a38a8b0479ab1055bef916db2452077e
OsziBen <osziben2@gmail.com>
2026-01-03 21:02:55 +0100

    Add partial board state class with bear off methods

---
+++ Domain/GameLogic/BoardState.BearOff.cs
@@ -0,0 +1,33 @@
+﻿using Common.Enums.BoardState;
+using Domain.GameLogic.Constants;
+
+namespace Domain.GameLogic
+{
+    public partial class BoardState
+    {
+        public bool CanBearOff(PlayerColor player)
+            => AllCheckersInHomeBoard(player);
+
+        public bool IsValidBearOffMove(
+            PlayerColor player,
+            int fromPoint,
+            int die)
+        {
+            if (!CanBearOff(player))
+            {
+                return false;
+            }
+
+            var target = player == PlayerColor.White
+                ? fromPoint + die
+                : fromPoint - die;
+
+            if (target == BoardConstants.OffBoardPosition)
+            {
+                return true;
+            }
+
+            return !HasCheckerOnHigherPoint(player, fromPoint);
+        }
+    }
+}
