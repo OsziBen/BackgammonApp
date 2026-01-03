3421bff13cfab3940a867f78e8087d9e9b35c681
OsziBen <osziben2@gmail.com>
2026-01-03 20:59:09 +0100

    Add interface for move sequence rules

---
+++ Domain/GameLogic/Rules/IMoveSequenceRule.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.GameLogic.Rules
+{
+    public interface IMoveSequenceRule
+    {
+        IEnumerable<MoveSequence> Apply(
+            IEnumerable<MoveSequence> sequences,
+            DiceRoll roll);
+    }
+}
