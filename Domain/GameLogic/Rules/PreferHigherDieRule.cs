6f89aabe798b943342637680d60ea80e50f64245
OsziBen <osziben2@gmail.com>
2026-01-03 21:01:31 +0100

    Add rule to ensure the higher move is choosen

---
+++ Domain/GameLogic/Rules/PreferHigherDieRule.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Domain.GameLogic.Rules
+{
+    public class PreferHigherDieRule : IMoveSequenceRule
+    {
+        public IEnumerable<MoveSequence> Apply(
+            IEnumerable<MoveSequence> sequences,
+            DiceRoll roll)
+        {
+            var list = sequences.ToList();
+
+            if (list.Count == 0)
+            {
+                return list;
+            }
+
+            var highestDie = list
+                .SelectMany(sequence => sequence.Moves)
+                .Max(move => move.Die);
+
+            return list
+                .Where(s =>
+                    s.Moves.Any(move => move.Die == highestDie));
+        }
+    }
+}
