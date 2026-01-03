06f7aeb5b14f460eee3ef0f3fdd5231d2daad1f8
OsziBen <osziben2@gmail.com>
2026-01-03 20:45:06 +0100

    Add board position and checker move logic constants

---
+++ Domain/GameLogic/Constants/BoardConstants.cs
@@ -0,0 +1,19 @@
+﻿using Common.Enums.BoardState;
+
+namespace Domain.GameLogic.Constants
+{
+    public static class BoardConstants
+    {
+        public const int BarPosition = 0;
+        public const int OffBoardPosition = -1;
+
+        public static bool IsHomeBoard(
+            int point,
+            PlayerColor player)
+        {
+            return player == PlayerColor.White
+                ? point >= 19 && point <= 24
+                : point >= 1 && point <= 6;
+        }
+    }
+}
