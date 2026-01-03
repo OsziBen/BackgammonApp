be4eebb47d31f40ce4e3c5ae5950f011103170aa
OsziBen <osziben2@gmail.com>
2026-01-03 21:06:30 +0100

    Add record for move sequence methods and props.

---
+++ Domain/GameLogic/MoveSequence.cs
@@ -0,0 +1,44 @@
+﻿namespace Domain.GameLogic
+{
+    public record MoveSequence(
+        IReadOnlyList<Move> Moves)
+    {
+        public virtual bool Equals(MoveSequence? other)
+        {
+            if (other is null)
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            if (Moves.Count != other.Moves.Count)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < Moves.Count; i++)
+            {
+                if (!Moves[i].Equals(other.Moves[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return Moves.Aggregate(
+                    17,
+                    (hash, move) => hash * 23 + move.GetHashCode());
+            }
+        }
+    }
+}
