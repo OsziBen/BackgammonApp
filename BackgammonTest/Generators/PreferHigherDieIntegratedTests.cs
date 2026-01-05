d899f5b76c2b43de1558898d74606460bbc3f0f6
OsziBen <osziben2@gmail.com>
2026-01-05 01:03:06 +0100

    Add test case for integrated rule in generator

---
+++ BackgammonTest/Generators/PreferHigherDieIntegratedTests.cs
@@ -0,0 +1,35 @@
+﻿using BackgammonTest.TestBuilders;
+using Common.Enums.BoardState;
+using Domain.GameLogic;
+using Domain.GameLogic.Generators;
+using FluentAssertions;
+
+namespace BackgammonTest.Generators
+{
+    public class PreferHigherDieIntegratedTests
+    {
+
+        [Theory]
+        [InlineData(PlayerColor.White)]
+        [InlineData(PlayerColor.Black)]
+        public void Generator_Enforces_Higher_Die_When_Available(PlayerColor player)
+        {
+            // Arrange
+            var state = BoardStateBuilder.Default()
+                .WithCurrentPlayer(player)
+                .WithChecker(player == PlayerColor.White ? 8 : 16, player)
+                .WithEnemyChecker(player == PlayerColor.White ? 16 : 8, player, 2)
+                .Build();
+
+            var dice = new DiceRoll(new[] { 3, 5 });
+            var generator = new MoveSequenceGenerator();
+
+            // Act
+            var sequences = generator.Generate(state, dice).ToList();
+
+            // Assert
+            sequences.Should().NotBeEmpty();
+            sequences.SelectMany(s => s.Moves).Should().OnlyContain(m => m.Die == 5);
+        }
+    }
+}
