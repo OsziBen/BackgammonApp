4d552d39c9c595d333a50d6675fe10c12a85cf0f
OsziBen <osziben2@gmail.com>
2026-01-09 20:34:39 +0100

    Adjust game session properties and relations

--- Domain/GameSession/GameSession.cs
+++ Domain/GameSession/GameSession.cs
@@ -5,26 +5,30 @@ namespace Domain.GameSession
 {
     public partial class GameSession : BaseEntity
     {
-        public Guid MatchId { get; set; }
-        public Guid CurrentGameId { get; set; }
+        public Guid? MatchId { get; set; }
+        public Guid? CurrentGameId { get; set; }
+        
         public string SessionCode { get; set; } = null!;
+        public GameSessionSettings Settings { get; set; } = null!;
 
         public GamePhase CurrentPhase { get; set; }
         public Guid? CurrentPlayerId { get; set; }
         public int[]? LastDiceRoll { get; set; }
-        public int? RemainingMoves { get; set; }
         public string? CurrentBoardStateJson { get; set; }
+
+        public int? DoublingCubeValue { get; set; }
+        public Guid? DoublingCubeOwnerPlayerId { get; set; }
 
         public DateTimeOffset CreatedAt { get; set; }
         public DateTimeOffset? StartedAt { get; set; }
         public DateTimeOffset? FinishedAt { get; set; }
         public DateTimeOffset LastUpdatedAt { get; set; }
-        public bool IsFinished { get; set; }
 
+        public bool IsFinished { get; set; }
         public Guid? WinnerPlayerId { get; set; }
 
-        public Match.Match Match { get; set; } = null!;
-        public Game.Game CurrentGame { get; set; } = null!;
+        public Match.Match? Match { get; set; }
+        public Game.Game? CurrentGame { get; set; }
         public GamePlayer.GamePlayer? WinnerPlayer { get; set; }
         public ICollection<GamePlayer.GamePlayer> Players { get; set; } = [];
     }
