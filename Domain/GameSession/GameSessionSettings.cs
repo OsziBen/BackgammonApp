df3f653e8bd18dd9a28d5b3da407b096d39ead1f
OsziBen <osziben2@gmail.com>
2026-01-09 20:37:32 +0100

    Add session settings model

---
+++ Domain/GameSession/GameSessionSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.GameSession
+{
+    public class GameSessionSettings
+    {
+        public int TargerPoints { get; init; } = 1;
+
+        public bool DoublingCubeEnabled { get; init; }
+
+        public bool ClockEnabled { get; init; }
+        public int? MatchTimePerPlayerInSeconds { get; init; }
+        public int? StartOfTurnDelayPerPlayerInSeconds { get; init; }
+
+        public bool CrawfordRuleEnabled { get; init; }
+    }
+}
