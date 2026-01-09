9a541f2ebb1183edaf37233238e6ca8417c25f36
OsziBen <osziben2@gmail.com>
2026-01-09 21:06:08 +0100

    Add validator with rules for creating game session

---
+++ Application/GameSessions/Commands/CreateGameSession/Validators/CreateGameSessionCommandValidator.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation;
+
+namespace Application.GameSessions.Commands.CreateGameSession.Validators
+{
+    public class CreateGameSessionCommandValidator : AbstractValidator<CreateGameSessionCommand>
+    {
+        public CreateGameSessionCommandValidator()
+        {
+            RuleFor(x => x.HostPlayerId)
+                .NotEmpty()
+                .WithMessage("Host player is required.");
+
+            RuleFor(x => x.Settings)
+                .NotNull()
+                .WithMessage("Game session settings are required.");
+
+            When(x => x.Settings != null, () =>
+            {
+                RuleFor(x => x.Settings.TargerPoints)
+                    .GreaterThan(0)
+                    .WithMessage("Target points must be greater than zero.");
+
+                RuleFor(x => x.Settings)
+                    .Must(s => !s.ClockEnabled || s.MatchTimePerPlayerInSeconds.HasValue)
+                    .WithMessage("Match time per player is required when clock is enabled.");
+
+                RuleFor(x => x.Settings)
+                    .Must(s => !s.ClockEnabled || s.MatchTimePerPlayerInSeconds > 0)
+                    .WithMessage("Match time per player must be greater than zero.");
+
+                RuleFor(x => x.Settings)
+                    .Must(s => !s.ClockEnabled ||
+                               !s.StartOfTurnDelayPerPlayerInSeconds.HasValue ||
+                               s.StartOfTurnDelayPerPlayerInSeconds >= 0)
+                    .WithMessage("Start of turn delay must be zero or positive.");
+            });
+        }
+    }
+}
