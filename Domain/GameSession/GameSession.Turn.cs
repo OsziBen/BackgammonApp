cfc7fe4664b6b2aaaa4b4570b5cc32ca7fa03847
OsziBen <osziben2@gmail.com>
2026-01-09 20:36:20 +0100

    Add partial class with session end turn method

---
+++ Domain/GameSession/GameSession.Turn.cs
@@ -0,0 +1,27 @@
+﻿using Common.Enums.GameSession;
+
+namespace Domain.GameSession
+{
+    public partial class GameSession
+    {
+        public void EndTurn()
+        {
+            if (Players.Count != 2)
+            {
+                throw new InvalidOperationException("Cannot end turn without 2 players");   // TODO: custom exception
+            }
+
+            var currentPlayer = Players.Single(p => p.Id == CurrentPlayerId);
+
+            var nextPlayer = Players.Single(p => p.Id != currentPlayer.Id);
+
+            CurrentPlayerId = nextPlayer.Id;
+
+            LastDiceRoll = null;
+
+            CurrentPhase = GamePhase.RollDice;
+
+            LastUpdatedAt = DateTimeOffset.UtcNow;
+        }
+    }
+}
