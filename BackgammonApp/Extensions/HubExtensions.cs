a1935d444a2ce5c095b9453cf3c40cd5c0e077b2
OsziBen <osziben2@gmail.com>
2026-01-09 20:43:18 +0100

    Add hub extension to get current player id

---
+++ BackgammonApp/Extensions/HubExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Application.Realtime.Connections;
+using Microsoft.AspNetCore.SignalR;
+using System.Security.Claims;
+
+namespace WebAPI.Extensions
+{
+    public static class HubExtensions
+    {
+        public static Guid GetCurrentPlayerId(
+            this Hub hub,
+            IConnectionMapping connections)
+        {
+            var claimValue = hub.Context.User?
+                .FindFirst(ClaimTypes.NameIdentifier)?
+                .Value;
+
+            if (Guid.TryParse(claimValue, out var playerId))
+            {
+                return playerId;
+            }
+
+            if (connections.TryGet(
+                hub.Context.ConnectionId,
+                out var mappedPlayerId))
+            {
+                return mappedPlayerId;
+            }
+
+            throw new HubException(
+                "Caller is not associated with a player");
+        }
+    }
+}
