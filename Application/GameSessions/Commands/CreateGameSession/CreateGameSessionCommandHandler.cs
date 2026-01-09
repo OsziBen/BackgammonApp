b533ca706e463237b6c7bd86772ecc906c885a68
OsziBen <osziben2@gmail.com>
2026-01-09 21:07:00 +0100

    Refactor code, adjust logic, add session code gen.

--- Application/GameSessions/Commands/CreateGameSession/CreateGameSessionCommandHandler.cs
+++ Application/GameSessions/Commands/CreateGameSession/CreateGameSessionCommandHandler.cs
@@ -1,4 +1,5 @@
-﻿using Application.Interfaces;
+﻿using Application.GameSessions.Services.SessionCodeGenerator;
+using Application.Interfaces;
 using Domain.GameSession;
 using MediatR;
 
@@ -7,19 +8,33 @@ namespace Application.GameSessions.Commands.CreateGameSession
     public class CreateGameSessionCommandHandler : IRequestHandler<CreateGameSessionCommand, Guid>
     {
         private readonly IUnitOfWork _uow;
+        private readonly ISessionCodeGenerator _sessionCodeGenerator;
 
-        public CreateGameSessionCommandHandler(IUnitOfWork uow)
+        public CreateGameSessionCommandHandler(
+            IUnitOfWork uow,
+            ISessionCodeGenerator sessionCodeGenerator)
         {
             _uow = uow;
+            _sessionCodeGenerator = sessionCodeGenerator;
         }
 
         public async Task<Guid> Handle(
             CreateGameSessionCommand request,
             CancellationToken cancellationToken)
         {
+            var hasActiveSession = await _uow.GameSessions.HasActiveSession(request.HostPlayerId);
+
+            if (hasActiveSession) {
+                throw new InvalidOperationException(    // TODO: custom exception
+                    "Player already has an active game session.");
+            }
+
+            var sessionCode = _sessionCodeGenerator.Generate();
+
             var session = GameSessionFactory.Create(
-                request.MatchId,
-                request.SessionCode);
+                request.HostPlayerId,
+                sessionCode,
+                request.Settings);
 
             await _uow.GameSessions.AddAsync(session);
             await _uow.CommitAsync();
