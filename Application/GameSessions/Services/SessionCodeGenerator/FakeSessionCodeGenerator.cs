98a162f360787afb3cb8618e057b222ebaa3670a
OsziBen <osziben2@gmail.com>
2026-01-09 20:49:21 +0100

    Add fake session code generator for testing

---
+++ Application/GameSessions/Services/SessionCodeGenerator/FakeSessionCodeGenerator.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.GameSessions.Services.SessionCodeGenerator
+{
+    public class FakeSessionCodeGenerator : ISessionCodeGenerator
+    {
+        public string Generate()
+            => "TEST01";
+    }
+}
