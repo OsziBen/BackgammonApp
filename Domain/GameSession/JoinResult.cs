5af6f5b1e4b192c397e87dba7d65cf80fa9f31e1
OsziBen <osziben2@gmail.com>
2026-01-10 17:42:37 +0100

    Add join result class, to handle join and rejoin

---
+++ Domain/GameSession/JoinResult.cs
@@ -0,0 +1,22 @@
+﻿namespace Domain.GameSession
+{
+    public class JoinResult
+    {
+        public GamePlayer.GamePlayer Player { get; }
+        public bool IsRejoin { get; }
+
+        private JoinResult(
+            GamePlayer.GamePlayer player,
+            bool isRejoin)
+        {
+            Player = player;
+            IsRejoin = isRejoin;
+        }
+
+        public static JoinResult Joined(GamePlayer.GamePlayer player)
+            => new(player, isRejoin: false);
+
+        public static JoinResult Rejoined(GamePlayer.GamePlayer player)
+            => new(player, isRejoin: true);
+    }
+}
