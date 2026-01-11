5c9413d26a27cdab97563007f5fce6f142b16414
OsziBen <osziben2@gmail.com>
2026-01-11 23:03:41 +0100

    Move fake session code generator to test layer

---
+++ BackgammonTest/GameSessions/Shared/FakeSessionCodeGenerator.cs
@@ -0,0 +1,10 @@
+﻿using Application.GameSessions.Services.SessionCodeGenerator;
+
+namespace BackgammonTest.GameSessions.Shared
+{
+    public class FakeSessionCodeGenerator : ISessionCodeGenerator
+    {
+        public string Generate()
+            => "TEST23";
+    }
+}
