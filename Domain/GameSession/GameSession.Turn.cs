97a386f1e182f37defa8dc9773bc3aa00900b715
OsziBen <osziben2@gmail.com>
2026-01-15 20:40:18 +0100

    Add determine starting player method remove end turn

--- Domain/GameSession/GameSession.Turn.cs
+++ Domain/GameSession/GameSession.Turn.cs
@@ -1,26 +1,57 @@
-﻿using Common.Enums.GameSession;
+﻿using Common.Enums;
+using Common.Enums.GameSession;
+using Common.Exceptions;
+using Domain.GameSession.Results;
 
 namespace Domain.GameSession
 {
     public partial class GameSession
     {
-        public void EndTurn(DateTimeOffset now)
+        public StartingPlayerResult DetermineStartingPlayer(
+            int roll1,
+            int roll2,
+            DateTimeOffset now)
         {
-            if (Players.Count != 2)
+            EnsureCanDetermineStartingPlayer();
+
+            if (roll1 == roll2)
             {
-                throw new InvalidOperationException("Cannot end turn without 2 players");   // TODO: custom exception
+                throw new BusinessRuleException(
+                    FunctionCode.BusinessRuleViolation,
+                    "Starting rolls must be distinct");
             }
 
-            var currentPlayer = Players.Single(p => p.Id == CurrentPlayerId);
+            var p1 = Players.ElementAt(0);
+            var p2 = Players.ElementAt(1);
 
+            p1.StartingRoll = roll1;
+            p2.StartingRoll = roll2;
+
+            var startingPlayer = roll1 > roll2 ? p1 : p2;
+
+            CurrentPlayerId = startingPlayer.Id;
+            CurrentPhase = GamePhase.MoveCheckers;
+            LastUpdatedAt = now;
+
+            return new StartingPlayerResult(
+                startingPlayer.Id,
+                new[]
+                {
+                    (p1.Id, roll1),
+                    (p2.Id, roll2)
+                });
+        }
+
+        public void EndTurn(DateTimeOffset now)
+        {
+            EnsureCanEndTurn();
+
+            var currentPlayer = Players.Single(p => p.Id == CurrentPlayerId);
             var nextPlayer = Players.Single(p => p.Id != currentPlayer.Id);
 
             CurrentPlayerId = nextPlayer.Id;
-
             LastDiceRoll = null;
-
             CurrentPhase = GamePhase.RollDice;
-
             LastUpdatedAt = now;
         }
     }
