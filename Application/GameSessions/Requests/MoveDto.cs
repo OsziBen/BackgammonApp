ba9182863ede9899329152eae147a37b2357d290
OsziBen <osziben2@gmail.com>
2026-01-15 20:15:49 +0100

    Add constructor method to dto

--- Application/GameSessions/Requests/MoveDto.cs
+++ Application/GameSessions/Requests/MoveDto.cs
@@ -5,5 +5,12 @@
         public int From { get; init; }
         public int To { get; init; }
         public int Die { get; init; }
+
+        public MoveDto(int from, int to, int die)
+        {
+            From = from;
+            To = to;
+            Die = die;
+        }
     }
 }
