fa117d802eed7ddf7ab07a4c80fd2f78a4a65adb
OsziBen <osziben2@gmail.com>
2026-01-15 20:41:40 +0100

    Add game result evaluation class and logic

---
+++ Domain/GameSession/GameResultEvaluator.cs
@@ -0,0 +1,36 @@
+﻿using Common.Enums.BoardState;
+using Common.Enums.Game;
+using Domain.GameLogic;
+
+namespace Domain.GameSession
+{
+    public static class GameResultEvaluator
+    {
+        public static GameResultType Evaluate(
+            BoardState boardState,
+            PlayerColor winnerColor,
+            int? doublingCubeValue)
+        {
+            if (!boardState.TryEvaluateVictory(winnerColor, out var baseResult))
+            {
+                baseResult = GameResultType.SimpleVictory;
+            }
+
+            return ApplyDoublingCube(baseResult, doublingCubeValue);
+        }
+
+        private static GameResultType ApplyDoublingCube(
+            GameResultType result,
+            int? cubeValue)
+        {
+            if (cubeValue == null || cubeValue == 1)
+            {
+                return result;
+            }
+
+            // TODO: multiply logic
+
+            return result;
+        }
+    }
+}
