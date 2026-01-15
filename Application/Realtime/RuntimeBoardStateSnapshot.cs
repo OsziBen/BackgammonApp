a0f08b6bd3d785ac5e2447e2ff99d6892f623bee
OsziBen <osziben2@gmail.com>
2026-01-15 20:18:50 +0100

    Set props to init, remove deprecated mapper method

--- Application/Realtime/RuntimeBoardStateSnapshot.cs
+++ Application/Realtime/RuntimeBoardStateSnapshot.cs
@@ -4,28 +4,15 @@ using Domain.GameLogic;
 
 namespace Application.Realtime
 {
-    public class RuntimeBoardStateSnapshot
+    public sealed class RuntimeBoardStateSnapshot
     {
-        public Dictionary<int, CheckerPosition> Points { get; set; } = [];
-        public int BarWhite { get; set; }
-        public int BarBlack { get; set; }
-        public int OffWhite { get; set; }
-        public int OffBlack { get; set; }
-        public PlayerColor CurrentPlayer { get; set; }
-        public GamePhase CurrentGamePhase { get; set; }
-        public DiceRoll? DiceRoll { get; set; }
-
-        public static RuntimeBoardStateSnapshot FromDomain(BoardState state)
-            => new()
-            {
-                Points = state.Points.ToDictionary(
-                    p => p.Key,
-                    p => p.Value.Clone()),
-                BarWhite = state.BarWhite,
-                BarBlack = state.BarBlack,
-                OffWhite = state.OffWhite,
-                OffBlack = state.OffBlack,
-                CurrentPlayer = state.CurrentPlayer
-            };
+        public Dictionary<int, CheckerPosition> Points { get; init; } = [];
+        public int BarWhite { get; init; }
+        public int BarBlack { get; init; }
+        public int OffWhite { get; init; }
+        public int OffBlack { get; init; }
+        public PlayerColor CurrentPlayer { get; init; }
+        public GamePhase CurrentGamePhase { get; init; }
+        public IReadOnlyList<int>? DiceRoll { get; init; }
     }
 }
