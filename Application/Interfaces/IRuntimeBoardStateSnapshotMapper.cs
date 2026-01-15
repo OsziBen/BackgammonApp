b17f14c65f03a42d44bf2be7f9d4d5e7a21095f2
OsziBen <osziben2@gmail.com>
2026-01-15 20:16:12 +0100

    Add interface for runtime snapshot mapper

---
+++ Application/Interfaces/IRuntimeBoardStateSnapshotMapper.cs
@@ -0,0 +1,11 @@
+﻿using Application.Realtime;
+using Domain.GameLogic;
+using Domain.GameSession;
+
+namespace Application.Interfaces
+{
+    public interface IRuntimeBoardStateSnapshotMapper
+    {
+        RuntimeBoardStateSnapshot Map(GameSession session, BoardState boardState);
+    }
+}
