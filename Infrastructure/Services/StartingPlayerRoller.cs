0fae0b3e1fb7c742aa2f0a617068c42074d3cd09
OsziBen <osziben2@gmail.com>
2026-01-17 00:10:38 +0100

    Add dice roll service to get starting player rolls

---
+++ Infrastructure/Services/StartingPlayerRoller.cs
@@ -0,0 +1,29 @@
+﻿using Application.Interfaces;
+using Domain.GameSession;
+
+namespace Infrastructure.Services
+{
+    public class StartingPlayerRoller : IStartingPlayerRoller
+    {
+        private readonly IDiceService _diceService;
+
+        public StartingPlayerRoller(IDiceService diceService)
+        {
+            _diceService = diceService;
+        }
+
+        public StartingPlayerRoll Roll()
+        {
+            int r1, r2;
+
+            do
+            {
+                r1 = _diceService.Roll();
+                r2 = _diceService.Roll();
+            }
+            while (r1 == r2);
+
+            return new StartingPlayerRoll(r1, r2);
+        }
+    }
+}
