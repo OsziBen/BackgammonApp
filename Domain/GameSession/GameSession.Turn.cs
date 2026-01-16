baa4fb23b5b938c624a1535c3673335f5b336da0
OsziBen <osziben2@gmail.com>
2026-01-17 00:19:26 +0100

    Remove starting player method, change single to first

--- Domain/GameSession/GameSession.Turn.cs
+++ Domain/GameSession/GameSession.Turn.cs
@@ -7,47 +7,12 @@ namespace Domain.GameSession
 {
     public partial class GameSession
     {
-        public StartingPlayerResult DetermineStartingPlayer(
-            int roll1,
-            int roll2,
-            DateTimeOffset now)
-        {
-            EnsureCanDetermineStartingPlayer();
-
-            if (roll1 == roll2)
-            {
-                throw new BusinessRuleException(
-                    FunctionCode.BusinessRuleViolation,
-                    "Starting rolls must be distinct");
-            }
-
-            var p1 = Players.ElementAt(0);
-            var p2 = Players.ElementAt(1);
-
-            p1.StartingRoll = roll1;
-            p2.StartingRoll = roll2;
-
-            var startingPlayer = roll1 > roll2 ? p1 : p2;
-
-            CurrentPlayerId = startingPlayer.Id;
-            CurrentPhase = GamePhase.MoveCheckers;
-            LastUpdatedAt = now;
-
-            return new StartingPlayerResult(
-                startingPlayer.Id,
-                new[]
-                {
-                    (p1.Id, roll1),
-                    (p2.Id, roll2)
-                });
-        }
-
         public void EndTurn(DateTimeOffset now)
         {
             EnsureCanEndTurn();
 
-            var currentPlayer = Players.Single(p => p.Id == CurrentPlayerId);
-            var nextPlayer = Players.Single(p => p.Id != currentPlayer.Id);
+            var currentPlayer = Players.First(p => p.Id == CurrentPlayerId);
+            var nextPlayer = Players.First(p => p.Id != currentPlayer.Id);
 
             CurrentPlayerId = nextPlayer.Id;
             LastDiceRoll = null;
