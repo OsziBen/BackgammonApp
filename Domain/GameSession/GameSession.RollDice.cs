48ec9d677b02c5b21809a8d3252ad9334afe5aba
OsziBen <osziben2@gmail.com>
2026-01-17 00:18:05 +0100

    Add roll dice partial class, implement domain logic

---
+++ Domain/GameSession/GameSession.RollDice.cs
@@ -0,0 +1,28 @@
+﻿using Common.Enums.GameSession;
+using Domain.GameLogic;
+
+namespace Domain.GameSession
+{
+    public partial class GameSession
+    {
+        public DiceRoll RollDice(
+            Guid playerId,
+            IDiceRoller diceRoller,
+            DateTimeOffset now)
+        {
+            EnsureNotFinished();
+            EnsurePhase(GamePhase.RollDice);
+            EnsureNoActiveDice();
+            EnsureCurrentPlayerIsSet();
+            EnsureCurrentPlayer(playerId);
+
+            var diceRoll = diceRoller.Roll();
+
+            LastDiceRoll = diceRoll.Values.ToArray();
+            CurrentPhase = GamePhase.MoveCheckers;
+            LastUpdatedAt = now;
+
+            return diceRoll;
+        }
+    }
+}
