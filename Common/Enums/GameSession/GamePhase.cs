38cb5e3e2f369538128f2ff2739454982cf1ec05
OsziBen <osziben2@gmail.com>
2026-01-22 15:52:00 +0100

    Adjust game phases for doubling cube logic

--- Common/Enums/GameSession/GamePhase.cs
+++ Common/Enums/GameSession/GamePhase.cs
@@ -5,12 +5,11 @@
         WaitingForPlayers = 0,
         DeterminingStartingPlayer = 1,
 
+        TurnStart = 9,
         RollDice = 10,
         MoveCheckers = 11,
 
         CubeOffered = 20,
-        CubeAccepted = 21,
-        CubeDeclined = 22,
 
         GameAbandoned = 98,
         GameFinished = 99
