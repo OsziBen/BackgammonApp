add6756875286151911db031e80c913853c5fc9e
OsziBen <osziben2@gmail.com>
2026-01-22 15:55:40 +0100

    Add command handler for offering doubling cube

---
+++ Application/GameSessions/Commands/OfferDoublingCube/OfferDoublingCubeCommandHandler.cs
@@ -0,0 +1,50 @@
+﻿using Application.GameSessions.Realtime;
+using Application.Interfaces;
+using Application.Shared;
+using Application.Shared.Time;
+using Domain.GameSession;
+using MediatR;
+
+namespace Application.GameSessions.Commands.OfferDoublingCube
+{
+    public class OfferDoublingCubeCommandHandler : IRequestHandler<OfferDoublingCubeCommand, Unit>
+    {
+        private readonly IUnitOfWork _uow;
+        private readonly IGameSessionNotifier _gameSessionNotifier;
+        private readonly IDateTimeProvider _timeProvider;
+
+        public OfferDoublingCubeCommandHandler(
+            IUnitOfWork uow,
+            IGameSessionNotifier gameSessionNotifier,
+            IDateTimeProvider timeProvider)
+        {
+            _uow = uow;
+            _gameSessionNotifier = gameSessionNotifier;
+            _timeProvider = timeProvider;
+        }
+
+        public async Task<Unit> Handle(
+            OfferDoublingCubeCommand request,
+            CancellationToken cancellationToken)
+        {
+            var session = await _uow.GameSessions
+                .GetByIdAsync(request.SessionId, asNoTracking: false)
+                .GetOrThrowAsync(nameof(GameSession), request.SessionId);
+
+            var now = _timeProvider.UtcNow;
+
+            var result = session.OfferDoublingCube(
+                request.PlayerId,
+                now);
+
+            await _uow.CommitAsync();
+
+            await _gameSessionNotifier.DoublingCubeOffered(
+                session.Id,
+                result.OfferingPlayerId,
+                result.OfferedCubeValue);
+
+            return Unit.Value;
+        }
+    }
+}
