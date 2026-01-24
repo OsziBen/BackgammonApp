e18e8adddc070b70db4cb5ebee9b0ecde839c517
OsziBen <osziben2@gmail.com>
2026-01-24 22:31:11 +0100

    Add read repository interface for game sessions

---
+++ Application/Interfaces/Repository/GameSession/IGameSessionReadRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Interfaces.Repository.GameSession
+{
+    public interface IGameSessionReadRepository
+    {
+        Task<Domain.GameSession.GameSession?> GetByIdAsync(Guid id);
+        Task<Domain.GameSession.GameSession?> GetBySessionCodeAsync(string sessionCode);
+        Task<bool> HasActiveSession(Guid playerId);
+    }
+}
