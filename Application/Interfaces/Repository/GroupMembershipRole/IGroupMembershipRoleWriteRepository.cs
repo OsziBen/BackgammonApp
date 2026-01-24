8a0faa576fe8d2cde65c7ea3271a933c930041a0
OsziBen <osziben2@gmail.com>
2026-01-24 22:31:39 +0100

    Add write repository interface for group memb. roles

---
+++ Application/Interfaces/Repository/GroupMembershipRole/IGroupMembershipRoleWriteRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Interfaces.Repository.GroupMembershipRole
+{
+    public interface IGroupMembershipRoleWriteRepository
+    {
+        Task<Domain.GroupMembershipRole.GroupMembershipRole?> GetByIdsAsync(Guid groupMembershipId, Guid groupRoleId);
+        Task AddAsync(Domain.GroupMembershipRole.GroupMembershipRole entity);
+        void Remove(Domain.GroupMembershipRole.GroupMembershipRole entity);
+    }
+}
