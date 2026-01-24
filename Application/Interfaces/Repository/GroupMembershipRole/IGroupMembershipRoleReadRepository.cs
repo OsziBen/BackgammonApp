65518c3c8cffa4fc2deb78f4bf4a154dd85dc90f
OsziBen <osziben2@gmail.com>
2026-01-24 22:30:54 +0100

    Add read repository interface for group memb. roles

---
+++ Application/Interfaces/Repository/GroupMembershipRole/IGroupMembershipRoleReadRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Interfaces.Repository.GroupMembershipRole
+{
+    public interface IGroupMembershipRoleReadRepository
+    {
+        Task<Domain.GroupMembershipRole.GroupMembershipRole?> GetByIdsAsync(Guid groupMembershipId, Guid groupRoleId);
+    }
+}
