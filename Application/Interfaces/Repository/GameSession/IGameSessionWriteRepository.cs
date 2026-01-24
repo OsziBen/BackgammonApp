8b9bf60de2654caa4de3317cdbf71a989c6a74a3
OsziBen <osziben2@gmail.com>
2026-01-24 22:31:53 +0100

    Add write repository interface for game sessions

---
+++ Application/Interfaces/Repository/GameSession/IGameSessionWriteRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Interfaces.Repository.GameSession
+{
+    public interface IGameSessionWriteRepository
+    {
+        Task<Domain.GameSession.GameSession?> GetByIdAsync(Guid id);
+        Task AddAsync(Domain.GameSession.GameSession session);
+        void Update(Domain.GameSession.GameSession session);
+        Task<Domain.GameSession.GameSession?> GetBySessionCodeAsync(string sessionCode, bool includePlayers = false);
+    }
+}
