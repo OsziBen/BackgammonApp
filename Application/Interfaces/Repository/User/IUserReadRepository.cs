c1a8c2a900b3e9a6abd8edaea335938e15898705
OsziBen <osziben2@gmail.com>
2026-01-24 22:30:39 +0100

    Add read repository interface for users

---
+++ Application/Interfaces/Repository/User/IUserReadRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Interfaces.Repository.User
+{
+    public interface IUserReadRepository
+    {
+        Task<Domain.User.User?> GetByEmailAsync(string email);
+        Task<bool> ExistsByUserNameAsync(string userName);
+    }
+}
