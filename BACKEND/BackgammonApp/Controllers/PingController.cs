7f854c9505d05300f526e5705cbba736a164c5b9
OsziBen <osziben2@gmail.com>
2026-01-28 23:08:04 +0100

    Add ping controller to test connection

---
+++ BACKEND/BackgammonApp/Controllers/PingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/ping")]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Ping() => Ok("pong");
+    }
+}
