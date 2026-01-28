3f76629da158c7ea59433d7d92882ac9eeb78278
OsziBen <osziben2@gmail.com>
2026-01-28 23:08:25 +0100

    Add extension method for CORS configuration

---
+++ BACKEND/BackgammonApp/Extensions/AppConfigExtensions.cs
@@ -0,0 +1,36 @@
+﻿namespace WebAPI.Extensions
+{
+    public static class AppConfigExtensions
+    {
+        private const string DevCorsPolicy = "DevCors";
+
+        public static IServiceCollection AddAppConfig(
+            this IServiceCollection services,
+            IConfiguration config)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(DevCorsPolicy, policy =>
+                {
+                    policy
+                    .WithOrigins(
+                        "http://localhost:4200",
+                        "https://localhost:4200"
+                    )
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials();
+                });
+            });
+
+            return services;
+        }
+
+        public static WebApplication ConfigureCORS(this WebApplication app)
+        {
+            app.UseCors(DevCorsPolicy);
+
+            return app;
+        }
+    }
+}
