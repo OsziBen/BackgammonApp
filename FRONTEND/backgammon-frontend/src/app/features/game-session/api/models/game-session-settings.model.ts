16f5bc39c28269f86b0eb8eb0051f112768c534b
OsziBen <osziben2@gmail.com>
2026-02-01 20:55:29 +0100

    Add game session settings model

---
+++ FRONTEND/backgammon-frontend/src/app/features/game-session/api/models/game-session-settings.model.ts
@@ -0,0 +1,10 @@
+export interface GameSessionSettings {
+  targetPoints: number;
+  doublingCubeEnabled: boolean;
+
+  clockEnabled: boolean;
+  matchTimePerPlayerInSeconds?: number | null;
+  startOfTurnDelayPerPlayerInSeconds?: number | null;
+
+  crawfordRuleEnabled: boolean;
+}
