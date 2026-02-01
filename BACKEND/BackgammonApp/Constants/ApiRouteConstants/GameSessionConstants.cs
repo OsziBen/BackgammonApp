eb2e1a79a2ed5f7199b92c2bf4b71d4533b9628d
OsziBen <osziben2@gmail.com>
2026-02-01 20:47:02 +0100

    Add api route constants for game session controller

---
+++ BACKEND/BackgammonApp/Constants/ApiRouteConstants/GameSessionConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace WebAPI.Constants.ApiRouteConstants
+{
+    public static class GameSessionConstants
+    {
+        public const string Base = "game-sessions";
+        public const string ById = "{sessionId:guid}";
+    }
+}
