bbcfa8b716ec1a80011443fa702d6c8549249884
OsziBen <osziben2@gmail.com>
2026-02-08 23:41:22 +0100

    Add get active session command and handler

---
+++ BACKEND/Application/GameSessions/Commands/GetActiveSessionByUserId/GetActiveSessionByUserIdCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using Application.GameSessions.Responses;
+using Application.Interfaces.Repository.GameSession;
+using MediatR;
+
+namespace Application.GameSessions.Commands.GetActiveSessionByUserId
+{
+    public class GetActiveSessionByUserIdCommandHandler : IRequestHandler<GetActiveSessionByUserIdCommand, GetActiveSessionByUserIdResponse?>
+    {
+        private readonly IGameSessionReadRepository _gameSessionReadRepository;
+
+        public GetActiveSessionByUserIdCommandHandler(IGameSessionReadRepository gameSessionReadRepository)
+        {
+            _gameSessionReadRepository = gameSessionReadRepository;
+        }
+
+        public async Task<GetActiveSessionByUserIdResponse?> Handle(GetActiveSessionByUserIdCommand request, CancellationToken cancellationToken)
+        {
+            // TODO: userId ellenőrzése
+
+            var session = await _gameSessionReadRepository.GetActiveByUserIdAsync(request.UserId);
+
+            return session == null ? null : new GetActiveSessionByUserIdResponse
+            {
+                SessionId = session.Id,
+                SessionCode = session.SessionCode,
+                Settings = session.Settings,
+                CreatedAt = session.CreatedAt
+            };
+        }
+    }
+}
