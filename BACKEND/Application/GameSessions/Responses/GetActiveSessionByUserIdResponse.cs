e1e0bd9e24c0613bd50cdaad08459f16232aff9f
OsziBen <osziben2@gmail.com>
2026-02-08 23:40:00 +0100

    Add game session response type

---
+++ BACKEND/Application/GameSessions/Responses/GetActiveSessionByUserIdResponse.cs
@@ -0,0 +1,12 @@
+﻿using Domain.GameSession;
+
+namespace Application.GameSessions.Responses
+{
+    public class GetActiveSessionByUserIdResponse
+    {
+        public Guid SessionId { get; set; }
+        public string SessionCode { get; set; } = null!;
+        public GameSessionSettings Settings { get; set; } = null!;
+        public DateTimeOffset CreatedAt { get; set; }
+    }
+}
