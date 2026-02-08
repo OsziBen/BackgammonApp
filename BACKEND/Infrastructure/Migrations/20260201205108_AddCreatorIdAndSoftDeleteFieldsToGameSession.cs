17a9781263ee577c7385b1807c5d6fbce7e22eda
OsziBen <osziben2@gmail.com>
2026-02-08 23:36:36 +0100

    Migrate database changes

---
+++ BACKEND/Infrastructure/Migrations/20260201205108_AddCreatorIdAndSoftDeleteFieldsToGameSession.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreatorIdAndSoftDeleteFieldsToGameSession : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CreatedByUserId",
+                table: "GameSessions",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DeletedAt",
+                table: "GameSessions",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "GameSessions",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedByUserId",
+                table: "GameSessions");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "GameSessions");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "GameSessions");
+        }
+    }
+}
---
+++ BACKEND/Infrastructure/Migrations/20260208194704_FixTargetPointsSettingsColumnName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixTargetPointsSettingsColumnName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Settings_TargerPoints",
+                table: "GameSessions",
+                newName: "Settings_TargetPoints");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Settings_TargetPoints",
+                table: "GameSessions",
+                newName: "Settings_TargerPoints");
+        }
+    }
+}
