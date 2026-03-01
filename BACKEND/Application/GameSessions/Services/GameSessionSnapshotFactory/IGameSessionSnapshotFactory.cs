c34224510c5c96449ba2ff31de0710b8fd0efbe9
OsziBen <osziben2@gmail.com>
2026-03-01 23:18:22 +0100

    Add snapshot factory method

---
+++ BACKEND/Application/GameSessions/Services/GameSessionSnapshotFactory/IGameSessionSnapshotFactory.cs
@@ -0,0 +1,13 @@
+﻿using Application.GameSessions.Responses;
+using Domain.GameSession;
+
+namespace Application.GameSessions.Services.GameSessionSnapshotFactory
+{
+    public interface IGameSessionSnapshotFactory
+    {
+        GameSessionSnapshotResponse Create(
+            GameSession session,
+            Guid? localPlayerId = null,
+            bool? isRejoin = false);
+    }
+}
