671cc9b8db75b5091ed269625609d32eebd39ba1
OsziBen <osziben2@gmail.com>
2026-03-01 22:47:32 +0100

    Add AppRole repository with methods

---
+++ BACKEND/Infrastructure/Repositories/AppRole/AppRoleReadRepository.cs
@@ -0,0 +1,17 @@
+﻿using Application.Interfaces.Repository.AppRole;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories.AppRole
+{
+    public class AppRoleReadRepository
+        : ReadRepositoryBase<Domain.AppRole.AppRole>,
+        IAppRoleReadRepository
+    {
+        public AppRoleReadRepository(ApplicationDbContext context) : base(context) { }
+
+        public async Task<Domain.AppRole.AppRole?> GetByNameAsync(string name, CancellationToken cancellationToken)
+            => await Query()
+                .FirstOrDefaultAsync(r => r.Name == name, cancellationToken);
+    }
+}
