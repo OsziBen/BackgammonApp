e1257b2405881535f046e86a5d993a9a20e893af
OsziBen <osziben2@gmail.com>
2026-03-01 23:08:38 +0100

    Add new constants, function codes and exception

---
+++ BACKEND/Common/Exceptions/UnauthorizedException.cs
@@ -0,0 +1,17 @@
+﻿using Common.Enums;
+
+namespace Common.Exceptions
+{
+    public class UnauthorizedException : ApplicationException
+    {
+        private const FunctionCode DefaultCode = FunctionCode.Unauthorized;
+
+        // Alapértelmezett konstruktor
+        public UnauthorizedException(string message)
+            : base(message, DefaultCode) { }
+
+        public UnauthorizedException(FunctionCode errorCode, string message)
+            : base(errorCode, message) { }
+
+    }
+}
