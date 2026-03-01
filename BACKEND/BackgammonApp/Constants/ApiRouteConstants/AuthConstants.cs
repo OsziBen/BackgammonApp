845bd98bdebd0aa730b3d4c5024d65cf680996e2
OsziBen <osziben2@gmail.com>
2026-03-01 23:21:28 +0100

    Add controller API route constants

---
+++ BACKEND/BackgammonApp/Constants/ApiRouteConstants/AuthConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI.Constants.ApiRouteConstants
+{
+    public static class AuthConstants
+    {
+        public const string Base = "auth";
+        public const string Login = "login";
+        public const string Logout = "logout";
+    }
+}
---
+++ BACKEND/BackgammonApp/Constants/ApiRouteConstants/UserConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI.Constants.ApiRouteConstants
+{
+    public static class UserConstants
+    {
+        public const string Base = "users";
+        public const string Register = "register";
+
+    }
+}
