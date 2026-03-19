b085656a75d4af915be459c09d2d2c3c867dd109
OsziBen <osziben2@gmail.com>
2026-03-19 22:42:21 +0100

    Adjust routes, add dashboard component and others

---
+++ FRONTEND/backgammon-frontend/src/app/features/forbidden/components/forbidden.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forbidden',
+  imports: [],
+  templateUrl: './forbidden.component.html',
+  styles: ``,
+})
+export class ForbiddenComponent {}
