e4caf3cfcbe53763c66c1caa05601fee8618b5ff
OsziBen <osziben2@gmail.com>
2026-04-28 22:40:49 +0200

    Add and register tournaments repository templates

---
+++ BACKEND/Infrastructure/Repositories/Tournament/TournamentWriteRepository.cs
@@ -0,0 +1,15 @@
+﻿using Application.Interfaces.Repository.Tournament;
+using Infrastructure.Data;
+
+namespace Infrastructure.Repositories.Tournament
+{
+    public class TournamentWriteRepository : ITournamentWriteRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TournamentWriteRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
