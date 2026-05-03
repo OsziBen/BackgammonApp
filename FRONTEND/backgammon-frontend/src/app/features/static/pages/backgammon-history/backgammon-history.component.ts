409f62100039c474a7ae3429ca8de6bbc1d95835
OsziBen <osziben2@gmail.com>
2026-05-03 23:57:56 +0200

    Add static page components and pages, add stylesheets

---
+++ FRONTEND/backgammon-frontend/src/app/features/static/pages/backgammon-history/backgammon-history.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'backgammon-history',
+  standalone: true,
+  templateUrl: './backgammon-history.component.html',
+  styleUrls: ['./backgammon-history.component.css'],
+})
+export class BackgammonHistoryComponent {}
