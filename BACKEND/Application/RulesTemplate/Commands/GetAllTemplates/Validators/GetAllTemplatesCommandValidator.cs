dd31b69c8a2219e3c51b4ef5c41db5cbea316218
OsziBen <osziben2@gmail.com>
2026-05-09 17:19:05 +0200

    Add rules templates commands, handlers and responses

---
+++ BACKEND/Application/RulesTemplate/Commands/GetAllTemplates/Validators/GetAllTemplatesCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Application.RulesTemplate.Commands.GetAllTemplates.Validators
+{
+    public class GetAllTemplatesCommandValidator : AbstractValidator<GetAllTemplatesCommand>
+    {
+        public GetAllTemplatesCommandValidator()
+        {
+
+        }
+    }
+}
